4e9fab0a00540471df0b06ead70103eab219f69d
Loch9 <70388529+Loch9@users.noreply.github.com>
2020-11-19 15:17:07 +0000

    Basic Logging
    Added basic logging.

--- Acrylic/src/Acrylic_src/EntryPoint.h
+++ Acrylic/src/Acrylic_src/EntryPoint.h
@@ -6,6 +6,11 @@ extern Acrylic::Application* Acrylic::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Acrylic::Log::Init();
+	ACR_CORE_ERROR("Log Initialized!");
+	int a = 76;
+	ACR_INFO("Hello! Var=[0]", a);
+
 	auto app = Acrylic::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Acrylic/src/Acrylic_src/Log.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Acrylic {
+
+	class ACRYLIC_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+#define ACR_CORE_TRACE(...)    ::Acrylic::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ACR_CORE_INFO(...)     ::Acrylic::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ACR_CORE_WARN(...)     ::Acrylic::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ACR_CORE_ERROR(...)    ::Acrylic::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ACR_CORE_CRITICAL(...) ::Acrylic::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#define ACR_TRACE(...)	       ::Acrylic::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ACR_INFO(...)	       ::Acrylic::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ACR_WARN(...)	       ::Acrylic::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ACR_ERROR(...)	       ::Acrylic::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ACR_CRITICAL(...)	   ::Acrylic::Log::GetClientLogger()->critical(__VA_ARGS__)
